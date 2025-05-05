60daade66208c10deb15735a73d79b2d259c2734
canton42 <145607162+canton42@users.noreply.github.com>
2025-05-05 18:24:49 -0400

    2. Change the method that populates the response hash map fillResponseMap to read from the file. responses.txt
    changed fillResponseMap method to read from responses.txt

--- Responder.java
+++ Responder.java
@@ -67,52 +67,54 @@ public class Responder
      */
     private void fillResponseMap()
     {
-        responseMap.put("crash", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("crashes", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("slow", 
-                        "I think this has to do with your hardware. Upgrading your processor\n" +
-                        "should solve all performance problems. Have you got a problem with\n" +
-                        "our software?");
-        responseMap.put("performance", 
-                        "Performance was quite adequate in all our tests. Are you running\n" +
-                        "any other processes in the background?");
-        responseMap.put("bug", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("buggy", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("windows", 
-                        "This is a known bug to do with the Windows operating system. Please\n" +
-                        "report it to Microsoft. There is nothing we can do about this.");
-        responseMap.put("macintosh", 
-                        "This is a known bug to do with the Mac operating system. Please\n" +
-                        "report it to Apple. There is nothing we can do about this.");
-        responseMap.put("expensive", 
-                        "The cost of our product is quite competitive. Have you looked around\n" +
-                        "and really compared our features?");
-        responseMap.put("installation", 
-                        "The installation is really quite straight forward. We have tons of\n" +
-                        "wizards that do all the work for you. Have you read the installation\n" +
-                        "instructions?");
-        responseMap.put("memory", 
-                        "If you read the system requirements carefully, you will see that the\n" +
-                        "specified memory requirements are 1.5 giga byte. You really should\n" +
-                        "upgrade your memory. Anything else you want to know?");
-        responseMap.put("linux", 
-                        "We take Linux support very seriously. But there are some problems.\n" +
-                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
-                        "more precise?");
-        responseMap.put("bluej", 
-                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
-                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
-                        "do about it, I'm afraid.");
+        Charset charset = Charset.forName("US-ASCII");
+        Path path = Paths.get("responses.txt");
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            StringBuilder response = new StringBuilder();
+            String[] currentKeys = null;
+            int blankLineCount = 0;
+            String line = reader.readLine();
+            while(line != null) {
+                if (line.trim().isEmpty()) {
+                    blankLineCount++;
+                    if (blankLineCount > 1) {
+                        throw new IOException("Two or more consecutive blank lines were encountered.");
+                    }
+                    if (currentKeys != null && response.length() > 0) {
+                        String responses = response.toString().trim();
+                        for (String key : currentKeys) {
+                            responseMap.put(key, responses);
+                        }
+                        response = new StringBuilder();
+                        currentKeys = null;
+                    }
+                } else {
+                    blankLineCount = 0;
+                    if (currentKeys == null) {
+                        currentKeys = line.split("\\s*,\\s*");
+                    } else {
+                        if (response.length() > 0) {
+                            response.append("\n");
+                        }
+                        response.append(line);
+                    }
+                }
+                line = reader.readLine();
+            }
+            if (currentKeys != null && response.length() > 0) {
+                String responses = response.toString().trim();
+                for (String key : currentKeys) {
+                    responseMap.put(key, responses);
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.err.println("Unable to open " + FILE_OF_DEFAULT_RESPONSES);
+        }
+        catch(IOException e) {
+            System.err.println("A problem was encountered reading " +
+                               "responses.txt");
+        }
     }
 
     /**
